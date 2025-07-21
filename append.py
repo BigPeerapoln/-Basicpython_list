0480d38cdf58f18f10af88b291ba837de315bb65
BigPeerapoln <s6806022510149@email.kmutnb.ac.th>
2025-07-21 21:17:42 +0700

    exam01

---
+++ append.py
@@ -0,0 +1,8 @@
+ # 1 --> append()
+name = ['test','ioio'] #การประกาศ list มีไว้ใช้เพื่อเก็บค่าข้อมูล
+string = input("enter your name:")
+age = int(input('enter your age:'))
+name.append(string)
+name.append(age) # สามารถเพิ่มตัวเลขเข้าไปได้
+print(name)
+
